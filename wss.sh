1ad5bb34a7542632277257298559a48cf04f79fb
Mahalakshmi Sankaran <mahalakshmi_sankaran@trimble.com>
2022-09-27 13:59:34 +0530

    Updated wss.sh with whitesource commands

--- wss.sh
+++ wss.sh
@@ -1,1 +1,52 @@
-echo "Goodbye"
+#!/bin/bash
+
+set -e
+
+if [ -z "$WS_APIKEY" ]; then
+  echo "You must set an API key!"
+  exit 126
+fi
+
+PROJECT_NAME_STR=""
+if [ -z "$INPUT_PROJECTNAME" ]; then
+  IFS='/' read -a GH_REPO <<< "$GITHUB_REPOSITORY"
+  PROJECT_NAME_STR="${GH_REPO[1]}"
+else
+  PROJECT_NAME_STR="$INPUT_PROJECTNAME"
+fi
+
+if [ -z "$INPUT_CONFIGFILE" ] && [ -z "$PROJECT_NAME_STR" ]; then
+  echo "'projectName' or 'configFile' path must be set."
+  exit 126
+fi
+
+PRODUCT_NAME_STR=""
+if [ -n "$INPUT_PRODUCTNAME" ]; then
+  PRODUCT_NAME_STR="-product $INPUT_PRODUCTNAME"
+fi
+
+
+# Download latest Unified Agent release from Whitesource
+curl -LJO  https://github.com/whitesource/unified-agent-distribution/releases/latest/download/wss-unified-agent.jar
+
+# verify jar signature
+jarsigner -verify  wss-unified-agent.jar
+
+
+#unset GOROOT passed automatically by setup-go
+unset GOROOT
+
+# don't exit if unified agent exits with error code
+set +e
+# Execute Unified Agent (2 settings)
+if [ -z  "$INPUT_CONFIGFILE" ]; then
+  java -jar wss-unified-agent.jar -noConfig true -apiKey $INPUT_APIKEY -project "$PROJECT_NAME_STR" $PRODUCT_NAME_STR\
+    -d . -wss.url $INPUT_WSSURL -resolveAllDependencies true
+else
+  java -jar wss-unified-agent.jar -apiKey $INPUT_APIKEY -c "$INPUT_CONFIGFILE" -d .
+fi
+
+WS_EXIT_CODE=$?
+echo "WS exit code: $WS_EXIT_CODE"
+
+exit $WS_EXIT_CODE
